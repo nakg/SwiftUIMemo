ad8ddf9b74aeae11c0b074516ee9bbe16c333a60
김낙진 <nakjin@mail.shinhan.dev>
2022-01-22 20:57:18 +0900

    first commit

---
+++ SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by 60067670 on 2022/01/22.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+		MemoListScene()
+    }
+}
