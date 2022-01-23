baedd2541952af9d5ed8cb97a1208bc2bbf11b78
김낙진 <nakjin@mail.shinhan.dev>
2022-01-23 09:50:03 +0900

    List Refactoring and Design Compose

---
+++ SwiftUIMemo/Scene/ComposeScene.swift
@@ -0,0 +1,52 @@
+//
+//  ComposeScene.swift
+//  SwiftUIMemo
+//
+//  Created by 60067670 on 2022/01/23.
+//
+
+import SwiftUI
+
+struct ComposeScene: View {
+	@EnvironmentObject var store: MemoStore
+	@State private var content: String = "" // 입력한 속성을 바인딩할 때 사용하는 속성 선언. 이런 속성들은 주로 @State로 선언.
+	
+    var body: some View {
+		NavigationView {
+			VStack {
+				TextField("", text: $content) // 이렇게하면 content속성과, textfield가 바인딩되고 텍스트필드에 문자를 입력하면 content 속성에 자동으로 저장된다. content 속성에 문자열을 저장하면 텍스트필드에도 반영된다. Swift UI는 이런 2-way 방식이 쉽다.
+			}
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.navigationBarTitle("새 메모", displayMode: .inline)
+			.navigationBarItems(leading: DismissButton(), trailing: SaveButton())
+		}
+    }
+}
+
+fileprivate struct DismissButton: View {
+	var body: some View {
+		Button {
+			
+		} label: {
+			Text("취소")
+		}
+	}
+}
+
+fileprivate struct SaveButton: View {
+	var body: some View {
+		Button {
+			
+		} label: {
+			Text("저장")
+		}
+
+	}
+}
+
+struct ComposeScene_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeScene()
+			.environmentObject(MemoStore()) // MemoStore를 커스텀 데이터르로 등록
+    }
+}
